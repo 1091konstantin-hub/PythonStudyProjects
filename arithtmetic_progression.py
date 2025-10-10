44ae3e51717a516b54cca43411e0f92d2715ab6f
1091konstantin-hub <1091konstantin@gmail.com>
2025-10-10 14:50:03 +0300

    Delete arithtmetic_progression.py

--- arithtmetic_progression.py
+++
@@ -1,6 +0,0 @@
-# Arithmetic progression calculator
-a1 = int(input())  # First number in progression
-d = int(input())  # Progression difference  
-n = int(input())  # Member number in progression
-an = a1 + d * (n - 1)  # Arithmetic progression formula
-print(an)  # Result
