23613dd437e19ff93d262276ce33854107cb24e5
1091konstantin-hub <1091konstantin@gmail.com>
2025-10-10 14:09:49 +0300

    Update arithtmetic_progression.py

--- arithtmetic_progression.py
+++ arithtmetic_progression.py
@@ -1,5 +1,6 @@
-a1 = int(input()) # первое число в прогрессии 
-d = int(input()) # разность прогресии
-n = int(input()) # номер члена прогрессии 
-an = a1+d*(n-1) # формула арифметической прогрессии
-print(an)
+# Arithmetic progression calculator
+a1 = int(input())  # First number in progression
+d = int(input())  # Progression difference  
+n = int(input())  # Member number in progression
+an = a1 + d * (n - 1)  # Arithmetic progression formula
+print(an)  # Result
