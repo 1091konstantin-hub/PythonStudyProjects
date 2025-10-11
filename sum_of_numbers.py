f3e817be5ed40956a84f3ab259899f3901f6516f
1091konstantin-hub <1091konstantin@gmail.com>
2025-10-11 23:50:19 +0300

    refactor: organaize all files into thematic folders

--- sum_of_numbers.py
+++
@@ -1,4 +0,0 @@
-# Program that calculates the sum of digits of a two-digit number
-number = int(input("Enter a number: "))  # Ask to enter a number
-formula = ((number // 10) + (number % 10))  # Formula calculates sum of digits
-print("Result: ", formula)  # Formula result
