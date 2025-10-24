3cf775d7b3e20b67299775115a007e9f74701854
1091konstantin-hub <1091konstantin@gmail.com>
2025-10-24 21:28:35 +0300

    feat: add min/max finder for five numbers

---
+++ algorithms/min_max_finder.py
@@ -0,0 +1,12 @@
+# Find smallest and largest number from five inputs
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+num4 = int(input())
+num5 = int(input())
+
+smallest = min(num1, num2, num3, num4, num5)
+largest = max(num1, num2, num3, num4, num5)
+
+print("Smallest number =", smallest)
+print("Largest number =", largest)
