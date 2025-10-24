5e3396f978491650471fc5076c90a04a52ab3902
1091konstantin-hub <1091konstantin@gmail.com>
2025-10-24 21:32:22 +0300

    feat: add sum of absolute values calculator

---
+++ basic_math/sum_of_absolutes.py
@@ -0,0 +1,9 @@
+# Calculate sum of absolute values of five numbers
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+num5 = float(input())
+
+sum_of_absolutes = abs(num1) + abs(num2) + abs(num3) + abs(num4) + abs(num5)
+print(sum_of_absolutes)
