1ee8e2833e571fd133e7d7ade711cb6f45fc181b
1091konstantin-hub <1091konstantin@gmail.com>
2025-10-24 21:42:02 +0300

    feat: add Manhattan distance calculator

---
+++ algorithms/manhattan_distance.py
@@ -0,0 +1,8 @@
+# Calculate Manhattan distance between two points
+p1 = int(input())
+p2 = int(input())
+q1 = int(input())
+q2 = int(input())
+
+manhattan_distance = abs(p1 - q1) + abs(p2 - q2)
+print(manhattan_distance)
